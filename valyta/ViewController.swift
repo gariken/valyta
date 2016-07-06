eb73cd5fc12a7ad874cb758cde61a2941e168af3
Alexander Goryatov <schet941@gmail.com>
2016-07-06 19:50:17 +0300

    Data

--- valyta/ViewController.swift
+++ valyta/ViewController.swift
@@ -17,40 +17,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        htmlData()
     }
     
     @IBAction func updateData(sender: AnyObject) {
         dispatch_async(dispatch_get_global_queue(QOS_CLASS_BACKGROUND, 0)){
             dispatch_async(dispatch_get_main_queue(), {
+                self.reloadData()
             })
         }
     }
     
-    /*
+    
     func reloadData(){
         let myURLString = "http://duzios.xyz"
         let myURL = NSURL(string: myURLString)
         do {
             let HTML = try String(contentsOfURL: myURL!)
-            let doc = Kanna.HTML(html: HTML, encoding: NSUTF8StringEncoding)
+            let doc = try HTMLDocument(string: HTML, encoding: NSUTF8StringEncoding)
             
-            for link in (doc?.xpath("//*[contains(@class, 'price')]/td[1]"))!{
-                print("ОБ Покупка - \(link.text)")
-                let pokupka = link.text
+            for link in (doc.xpath("//*[contains(@class, 'price')]/td[1]")){
+                print("ОБ Покупка - \(link.stringValue)")
+                let pokupka = link.stringValue
                 LabelPrice.text = pokupka
             }
             
-            for link in (doc?.xpath("//*[contains(@class, 'price')]/td[2]"))!{
-                print("ОБ Продажа - \(link.text)")
-                let prodaja = link.text
+            for link in (doc.xpath("//*[contains(@class, 'price')]/td[2]")){
+                print("ОБ Продажа - \(link.stringValue)")
+                let prodaja = link.stringValue
                 labelPurchase.text = prodaja
             }
             
-            for link in (doc?.xpath("//td[@class = 'time']"))!{
-                print("ОБ Дата - \(link.text)")
-                let date = link.text
-                labelDate.text = "Обновленно в - \(date!)"
+            for link in (doc.xpath("//td[@class = 'time']")){
+                print("ОБ Дата - \(link.stringValue)")
+                let date = link.stringValue
+                labelDate.text = "Обновленно в - \(date)"
                 
             }
             
@@ -63,31 +64,27 @@ class ViewController: UIViewController {
     
     func htmlData(){
         let myURLString = "http://duzios.xyz"
-        guard let myURL = NSURL(string: myURLString) else {
-            print("Error: \(myURLString) doesn't seem to be a valid URL")
-            return
-        }
-        
+        let myURL = NSURL(string: myURLString)
         do {
-            let HTML = try String(contentsOfURL: myURL)
-            let doc = Kanna.HTML(html: HTML, encoding: NSUTF8StringEncoding)
+            let HTML = try String(contentsOfURL: myURL!)
+            let doc = try HTMLDocument(string: HTML, encoding: NSUTF8StringEncoding)
             
-            for link in (doc?.xpath("//*[contains(@class, 'price')]/td[1]"))!{
-                print("Покупка - \(link.text)")
-                let pokupka = link.text
+            for link in (doc.xpath("//*[contains(@class, 'price')]/td[1]")){
+                print("ОБ Покупка - \(link.stringValue)")
+                let pokupka = link.stringValue
                 LabelPrice.text = pokupka
             }
             
-            for link in (doc?.xpath("//*[contains(@class, 'price')]/td[2]"))!{
-                print("Продажа - \(link.text)")
-                let prodaja = link.text
+            for link in (doc.xpath("//*[contains(@class, 'price')]/td[2]")){
+                print("ОБ Продажа - \(link.stringValue)")
+                let prodaja = link.stringValue
                 labelPurchase.text = prodaja
             }
             
-            for link in (doc?.xpath("//td[@class = 'time']"))!{
-                print("Дата - \(link.text)")
-                let date = link.text
-                labelDate.text = "Обновленно в - \(date!)"
+            for link in (doc.xpath("//td[@class = 'time']")){
+                print("ОБ Дата - \(link.stringValue)")
+                let date = link.stringValue
+                labelDate.text = "Обновленно в - \(date)"
                 
             }
             
@@ -96,7 +93,4 @@ class ViewController: UIViewController {
             print("Error: \(error)")
         }
     }
-   */
-}
-*/*/*/*/
 }
